613c10c8d47183952520f5d04a7dac6e22924900
BommakantiAlekhya <71487433+BommakantiAlekhya@users.noreply.github.com>
2020-12-22 22:01:47 +0530

    second copy

---
+++ src/cglgame.java
@@ -0,0 +1,31 @@
+
+		
+
+
+		public class cglgame {
+			int width;
+			int height;
+
+			public cglgame() {
+				// TODO Auto-generated constructor stub
+			
+			}
+
+			public int getWidth() {
+				return width;
+			}
+
+			public void setWidth(int width) {
+				this.width = width;
+			}
+
+			public int getHeight() {
+				return height;
+			}
+
+			public void setHeight(int height) {
+				this.height = height;
+			}
+
+		}
+
